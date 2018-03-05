91c72362ee78af6e3efc384a9564a0ac37da9973
Erik Schierboom <erik_schierboom@hotmail.com>
2018-03-05 11:51:05 +0100

    Add Travis deployment

--- build.sh
+++ build.sh
@@ -1,2 +1,9 @@
+npm install -g firebase-tools
+npm install --no-package-lock
+
 npm run build
-npm test
+npm run test
+
+if [ $TRAVIS_BRANCH == 'master' ]; then
+  npm run deploy
+fi
--- prepare.sh
+++
@@ -1,4 +0,0 @@
-npm dedupe
-npm prune
-npm install
-npm install -g firebase-tools
