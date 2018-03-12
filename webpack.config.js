636eda0768fe0495c714087c0ded6d2b64342b2e
Erik Schierboom <erik_schierboom@hotmail.com>
2018-03-12 08:40:45 +0100

    Move static assets to /public directory

--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
+      filename: "./index.html",
+      template: "./public/index.html"
     })
   ]
 };
