80896adaafb94f38da3c152ca898644c128cb8ba
Erik Schierboom <erik_schierboom@hotmail.com>
2018-03-12 12:32:54 +0100

    Add basic menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,59 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarItem,
+  Icon,
+  NavbarBurger,
+  NavbarMenu,
+  NavbarStart,
+  NavbarLink,
+  NavbarDropdown,
+  NavbarDivider,
+  NavbarEnd,
+  Field,
+  Control,
+  Button
+} from "bloomer";
 
-export const Header = () => (
-  <>
-    <h1>LoTRO completionist</h1>
-    <ul>
-      <li>
-        <Link to="/instances">Instances</Link>
-      </li>
-      <li>
-        <Link to="/raids">Raids</Link>
-      </li>
-    </ul>
-  </>
-);
+import icon from "../assets/icon.png";
+
+interface HeaderProps {}
+
+interface HeaderState {
+  isActive: boolean
+}
+
+export class Header extends React.Component<HeaderProps, HeaderState> {
+  constructor(props: HeaderProps) {
+    super(props);
+    this.state = { isActive: false };
+  }
+
+  onClickNav = () => {
+    this.setState({ isActive: !this.state.isActive });
+  }
+
+  render() {
+    return  (
+      <Navbar className="is-link">
+        <NavbarBrand>
+          <NavbarItem href="/">
+            <img src={icon} style={{ marginRight: 5 }} /> LoTRO completionist
+          </NavbarItem>
+          <NavbarBurger isActive={this.state.isActive} onClick={this.onClickNav}>
+            <span></span>
+            <span></span>
+          </NavbarBurger>
+        </NavbarBrand>
+        <NavbarMenu isActive={this.state.isActive} onClick={this.onClickNav}>
+          <NavbarStart>
+            <NavbarItem href="/instances">Instances</NavbarItem>
+            <NavbarItem href="/raids">Raids</NavbarItem>
+          </NavbarStart>
+        </NavbarMenu>
+      </Navbar>
+    );
+  }
+};
