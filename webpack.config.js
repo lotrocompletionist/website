cf34888fb7fa0d34ceb7a1c6a26d257d4f4e18b7
Erik Schierboom <erik_schierboom@hotmail.com>
2018-03-16 20:12:07 +0100

    Simplify usage of webpack environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,64 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const WebappWebpackPlugin = require("webapp-webpack-plugin");
 
-const production = process.env.NODE_ENV == "production";
-
-module.exports = {
-  devtool: "source-map",
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".json"]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "ts-loader"
-        }
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: "style-loader"
-          },
-          {
-            loader: "css-loader"
-          },
-          {
-            loader: "sass-loader",
+module.exports = function(env, argv) {
+  return {
+    devtool: "source-map",
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "ts-loader"
+          }
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            {
+              loader: "style-loader"
+            },
+            {
+              loader: "css-loader"
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                includePaths: ["node_modules"]
+              }
+            }
+          ]
+        },
+        {
+          test: /\.html$/,
+          use: {
+            loader: "html-loader",
             options: {
-              includePaths: ["node_modules"]
+              minimize: true
             }
           }
-        ]
-      },
-      {
-        test: /\.html$/,
-        use: {
-          loader: "html-loader",
+        },
+        {
+          test: /\.(png|jpg|gif)$/,
+          loader: "url-loader",
           options: {
-            minimize: true
+            limit: 8192
           }
         }
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        loader: "url-loader",
-        options: {
-          limit: 8192
-        }
-      }
+      ]
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        filename: "./index.html",
+        template: "./src/index.html"
+      }),
+      ...(argv.mode === "production"
+        ? [new WebappWebpackPlugin("./src/assets/icon.png")]
+        : [])
     ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      filename: "./index.html",
-      template: "./src/index.html"
-    }),
-    ...(production ? [new WebappWebpackPlugin("./src/assets/icon.png")] : [])
-  ]
+  };
 };
